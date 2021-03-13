1d6ade82ea1109cc82456cd87d520762481667c9
Abel Salas Leal <abelsl1999@gmail.com>
2021-03-12 22:37:35 -0600

    Se agregó la función run() al código de los dados

--- Juego-dados.py
+++ Juego-dados.py
@@ -1,12 +1,18 @@
 import random
-v_min=1
-v_max=6
-tirar='y'
 
-while tirar=='y':
-  print('Tirando los dados...')
-  dado1=random.randint(v_min,v_max)
-  dado2=random.randint(v_min,v_max)
-  print(f'El valor de los dados fueron {dado1} y {dado2}')
-  print(f'Sumando un valor de {dado1+dado2}')
-  tirar=input('¿Quieres tirar de nuevo? (y / n): ')
+
+def run():
+    v_min=1
+    v_max=6
+    tirar='y'
+    while tirar=='y':
+        print('Tirando los dados...')
+        dado1=random.randint(v_min,v_max)
+        dado2=random.randint(v_min,v_max)
+        print(f'El valor de los dados fueron {dado1} y {dado2}')
+        print(f'Sumando un valor de {dado1+dado2}')
+        tirar=input('¿Quieres tirar de nuevo? (y / n): ')
+
+
+if __name__ == '__main__':
+    run()
