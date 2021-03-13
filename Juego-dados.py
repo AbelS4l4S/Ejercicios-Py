f839e3ce6a7d4d4b850dd23fb93425470b96b905
Abel Salas Leal <abelsl1999@gmail.com>
2021-03-12 22:19:54 -0600

    Se actualizó código juego de dados

---
+++ Juego-dados.py
@@ -0,0 +1,12 @@
+import random
+v_min=1
+v_max=6
+tirar='y'
+
+while tirar=='y':
+  print('Tirando los dados...')
+  dado1=random.randint(v_min,v_max)
+  dado2=random.randint(v_min,v_max)
+  print(f'El valor de los dados fueron {dado1} y {dado2}')
+  print(f'Sumando un valor de {dado1+dado2}')
+  tirar=input('¿Quieres tirar de nuevo? (y / n): ')
